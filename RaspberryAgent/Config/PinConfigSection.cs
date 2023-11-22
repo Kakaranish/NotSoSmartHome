401447d5ed799ac261a7eb93d5ca7220c08a8be4
Kakaranish <stasiekgruz@gmail.com>
2023-11-22 21:21:19 +0100

    gpio controller adapters 2

--- RaspberryAgent/Config/PinConfigSection.cs
+++ RaspberryAgent/Config/PinConfigSection.cs
@@ -2,7 +2,7 @@ using System.Device.Gpio;
 
 namespace RaspberryAgent.Config;
 
-public class PinConfig
+public class PinConfigSection
 {
     public int Pin { get; set; }
     public PinMode Mode { get; set; }
--- RaspberryAgent/Config/RaspberryOptions.cs
+++ RaspberryAgent/Config/RaspberryOptions.cs
@@ -5,5 +5,5 @@ public class RaspberryOptions
     public static readonly string SectionName = "Raspberry";
 
     public bool UseFakeGpioController { get; set; } = false;
-    public PinConfig[] OpenedPins { get; set; } = Array.Empty<PinConfig>();
+    public PinConfigSection[] OpenedPins { get; set; } = Array.Empty<PinConfigSection>();
 }
